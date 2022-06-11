c0e1afd44bcd35b34df3ce9e2a983525c35fe153
Osthi <Osthi@gmail.com>
2022-06-11 19:29:36 +0530

    Raj Second Version

--- Greens.java
+++ Greens.java
@@ -1,6 +1,9 @@
 public class Greens {
     public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
         System.out.println("Hello students :)");
+        System.out.println(a+b);
         
     }
 }
